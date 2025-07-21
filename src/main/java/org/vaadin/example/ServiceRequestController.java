b42ab1ae4e5b7f3d255a9767189a3ce223ffdd53
ife-hub <76948294+ife-hub@users.noreply.github.com>
2025-07-21 14:21:19 +0100

    Update ServiceRequestController.java

--- src/main/java/org/vaadin/example/ServiceRequestController.java
+++ src/main/java/org/vaadin/example/ServiceRequestController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.vaadin.example.MSSQLTableReader;
-import org.vaadin.example.services.ServiceRequestService;
+import org.vaadin.example.ServiceRequestService;
 
 import java.util.*;
 
